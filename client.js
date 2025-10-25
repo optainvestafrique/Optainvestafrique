dd683c44ee8f3c36edbc0eafd85b4eff09af3154
optainvestafrique <cstanmavikana18@gmail.com>
2025-10-25 22:50:39 +0100

    Create client.js

---
+++ client.js
@@ -0,0 +1,9 @@
+const root = document.getElementById("root");
+
+root.innerHTML = `
+  <div style="text-align:center; padding:40px;">
+    <h1>Bienvenue sur Optainvest Afrique</h1>
+    <p>Votre plateforme d’investissement sécurisée.</p>
+    <p>Investissez dès aujourd’hui et faites croître vos revenus.</p>
+  </div>
+`;
